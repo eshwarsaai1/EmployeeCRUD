58f6f92bd75b91f31034a1f51fa9dfef556532b5
Eshwar <eshwarsaaigouru456@gmail.com>
2024-08-24 20:42:14 +0530

    initial commit

---
+++ src/main/java/com/training/CRUDEmployee/repository/impl/Database.java
@@ -0,0 +1,8 @@
+package com.training.CRUDEmployee.repository.impl;
+
+public class Database {
+    public final static String userName = "User";
+    public final static String password = "4326";
+    public final static String url = "jdbc:mysql://localhost:3306/employee_db";
+    public final static String ClassName = "com.mysql.cj.jdbc.Driver";
+}
